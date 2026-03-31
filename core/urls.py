13a6d2df4b71f8638dd45597537c10e190c2f6a4
lack0f <lack0f@mail.com>
2026-03-31 20:16:51 -0300

    feat: add hero section

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import home
+from django.urls import path, include
+from .views import home, CustomLoginView, editor, gestor
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('accounts/login/', CustomLoginView.as_view(), name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('editor/', editor, name='editor'),
+    path('gestor/', gestor, name='gestor'),
 ]
